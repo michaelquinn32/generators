1d90337df9a8f1fe18c222efaa611dc04d255a38
michaelquinn32 <michael.quinn@aya.yale.edu>
2016-04-30 21:39:18 -0400

    First commit; initialized package

---
+++ R/generators.R
@@ -0,0 +1,7 @@
+#' @seealso \url[iterators]{https://cran.r-project.org/web/packages/iterators/index.html} and \url[itertools]{https://cran.r-project.org/web/packages/itertools/index.html}
+#'
+#' @name generators
+#' @useDynLib generators
+#' @importFrom Rcpp sourceCpp
+
+"_PACKAGE"
